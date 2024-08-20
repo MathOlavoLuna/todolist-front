76e5765d0761662ee7782eefa760d8af8dcacdd2
Matheus Luna <srmatheus6162@gmail.com>
2024-08-19 23:10:38 -0300

    feat: tirei a navbar do app.vue e botei em um component.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="d-flex align-center justify-space-around py-3">
+    <RouterLink to="/">Home</RouterLink>
+    <img
+      width="64"
+      height="64"
+      src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/external-to-do-list-productivity-flaticons-lineal-color-flat-icons.png"
+      alt="external-to-do-list-productivity-flaticons-lineal-color-flat-icons"
+    />
+    <RouterLink to="/login" />
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  height: 100%;
+}
+
+nav > a {
+  text-decoration: none;
+}
+</style>
