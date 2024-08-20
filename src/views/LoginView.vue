d566c2f03b3a8c36d15c58567da66fc781a58932
Matheus Luna <srmatheus6162@gmail.com>
2024-08-19 23:16:54 -0300

    feat: crie a view de login.

---
+++ src/views/LoginView.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import NavBar from '@/components/NavBar.vue';
+import { login } from '@/services/api';
+import { checkLogged } from '@/utils/checkLogged';
+import { onMounted, ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const email = ref<string>('');
+const password = ref<string>('');
+const router = useRouter();
+
+async function handleLogin() {
+  const response = await login(email.value, password.value);
+
+  if (response) {
+    console.log('login efetuado');
+    router.push('/');
+  } else {
+    console.log('login mal sucedido');
+  }
+}
+
+onMounted(() => {
+  checkLogged('/');
+});
+</script>
+
+<template>
+  <NavBar />
+  <v-form>
+    <v-text-field label="email" v-model="email"> </v-text-field>
+    <v-text-field label="password" v-model="password"> </v-text-field>
+    <v-btn color="success" size="large" variant="elevated" block @click="handleLogin">
+      Sign In
+    </v-btn>
+  </v-form>
+</template>
+
+<style></style>
