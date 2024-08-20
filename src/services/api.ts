c68dc071b121ed33734bfbc71844114a3c5a3ad8
Matheus Luna <srmatheus6162@gmail.com>
2024-08-19 23:13:21 -0300

    feat: criei a rota de login, a rota de getTodos, a rota de postTodo

---
+++ src/services/api.ts
@@ -0,0 +1,60 @@
+import axios from 'axios';
+const client = axios.create({
+  baseURL: 'http://127.0.0.1:8000/api/',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+const config = {
+  headers: { Authorization: `Bearer ${sessionStorage.getItem('token')}` }
+};
+
+export const login = async (email: string, password: string) => {
+  try {
+    const response = await client.post('login', {
+      email,
+      password
+    });
+
+    if (response.status === 200) {
+      sessionStorage.setItem('token', response.data.data.token);
+      return true;
+    }
+    return false;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getTodo = async () => {
+  try {
+    const response = await client.get('toDo', config);
+
+    if (response.status === 200) {
+      return response.data.data;
+    }
+
+    return false;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const postTodo = async (title: string, content: string, priority: number) => {
+  const response = await client.post(
+    'toDo',
+    {
+      title,
+      content,
+      priority
+    },
+    config
+  );
+
+  if (response.status === 200) {
+    return true;
+  }
+
+  return false;
+};
