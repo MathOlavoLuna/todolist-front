86d2f9b417336161de11ad759f4c4b0252ddea2c
Matheus Luna <srmatheus6162@gmail.com>
2024-08-22 18:12:48 -0300

    feat: adicionei a função editDone que edita o atributo done da tabela todo

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,62 +1,86 @@
 import axios from 'axios';
 const client = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/',
-  headers: {
-    'Content-Type': 'application/json'
-  }
+ baseURL: 'http://127.0.0.1:8000/api/',
+ headers: {
+  'Content-Type': 'application/json'
+ }
 });
 
 const config = {
-  headers: { Authorization: `Bearer ${sessionStorage.getItem('token')}` }
+ headers: { Authorization: `Bearer ${sessionStorage.getItem('token')}` }
 };
 
 export const login = async (email: string, password: string) => {
-  try {
-    const response = await client.post('login', {
-      email,
-      password
-    });
-
-    if (response.status === 200) {
-      sessionStorage.setItem('token', response.data.data.token);
-      return true;
-    }
-    return false;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const getTodo = async () => {
-  try {
-    const response = await client.get('toDo', config);
-
-    if (response.status === 200) {
-      return response.data.data;
-    }
-
-    return false;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const postTodo = async (title: string, content: string, priority: string) => {
-  const response = await client.post(
-    'toDo',
-    {
-      title,
-      content,
-      priority
-    },
-    config
-  );
+ try {
+  const response = await client.post('login', {
+   email,
+   password
+  });
 
   if (response.status === 200) {
-    console.log(response);
+   sessionStorage.setItem('token', response.data.data.token);
+   return true;
+  }
+  return false;
+ } catch (error) {
+  console.log(error);
+ }
+};
 
-    return true;
+export const getToDo = async () => {
+ try {
+  const response = await client.get('toDo', config);
+
+  if (response.status === 200) {
+   return response.data.data;
   }
 
   return false;
+ } catch (error) {
+  console.log(error);
+ }
+};
+
+export const editDone = async (id: number, done: boolean) => {
+ try {
+  const response = await client.put(
+   `ToDo/${id}`,
+   {
+    done
+   },
+   config
+  );
+
+  if (response.status === 200) {
+   return 'Done Editado com sucesso' + response;
+  }
+
+  return 'Falha ao editar Done' + response;
+ } catch (error) {
+  console.log(error);
+ }
+};
+
+export const postTodo = async (title: string, content: string, priority: string) => {
+ try {
+  const response = await client.post(
+   'toDo',
+   {
+    title,
+    content,
+    priority
+   },
+   config
+  );
+
+  if (response.status === 200) {
+   console.log(response);
+
+   return true;
+  }
+
+  return false;
+ } catch (error) {
+  console.log(error);
+ }
 };
