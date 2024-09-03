e286f950461ccb85d96e457fa3bc1744166f54a2
Matheus Luna <srmatheus6162@gmail.com>
2024-09-02 22:53:50 -0300

    feat: ajustando o checkLogged para localStorage pq n aguento mas logar td vez que sub a aplicação pro ar

--- src/utils/checkLogged.ts
+++ src/utils/checkLogged.ts
@@ -1,11 +1,11 @@
 import { useRouter } from 'vue-router';
 
 export function checkLogged(url: string) {
-  const logged = sessionStorage.getItem('token');
-  const router = useRouter();
-  if (logged) {
-    router.push(url);
-  } else {
-    router.push('/login');
-  }
+ const logged = localStorage.getItem('token');
+ const router = useRouter();
+ if (logged) {
+  router.push(url);
+ } else {
+  router.push('/login');
+ }
 }
