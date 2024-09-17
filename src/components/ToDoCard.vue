37d054cc8e10c5e46cb988456cb13a83988a7d4b
Matheus Luna <srmatheus6162@gmail.com>
2024-09-17 14:03:13 -0300

    feat: Adicionei as funções de editar e deletar um dos toDos, e uma função para quando clicar na modal aparecer editar e o deletar

--- src/components/ToDoCard.vue
+++ src/components/ToDoCard.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
-import { editDone } from '@/services/api';
+import { editDone, getToDo, getToDoInfs, putToDo } from '@/services/api';
 import type { ToDoType } from '@/types';
+import { validatePriority } from '@/utils/validatePriority';
 import SvgIcon from '@jamescoyle/vue-icon';
 import { mdiSquare } from '@mdi/js';
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 interface toDoProps {
  toDo: ToDoType;
 }
 defineProps<toDoProps>();
+
+//Put ToDo
+const title = ref<string>('');
+const content = ref<string>('');
+const priority = ref<string>('');
+
+//Edit - Delete
+const showClouds = ref<boolean>(false);
 
 const path = mdiSquare;
 const doneCheck = ref<boolean>(false);
@@ -16,39 +25,86 @@ const emits = defineEmits(['callGetToDo']);
 
 async function handleEditDone(id: number) {
  const response = await editDone(id, !doneCheck.value); //negando o valor para que ele sempre faça o edit da maneira correta (tu anotou isso no word VUE)
- console.log(response);
+ if (response) emits('callGetToDo');
+}
 
- emits('callGetToDo');
+async function handleGetToDoInfs(id: number) {
+ const response = await getToDoInfs(id);
+ if (response) return (title.value = response.data.title), (content.value = response.data.content); // temos que fazer a lógica para puxar a prioridade também.
+}
+
+async function handlePutToDo(id: number) {
+ const validatedPriority = validatePriority(priority.value);
+ const response = await putToDo(id, title.value, content.value, validatedPriority);
+ if (response) emits('callGetToDo');
+}
+
+function showIcons() {
+ showClouds.value = !showClouds.value;
 }
 </script>
 
 <template>
- <v-col cols="12" lg="12">
-  <v-card class="mx-auto" max-width="1000" hover :class="{ 'opacity-50': toDo.done }">
-   <v-card-item>
-    <div class="d-flex align-center justify-space-between">
-     <div class="text-lg-h5 text-phone">{{ toDo.title }}</div>
+ <v-row class="d-flex justify-center aling-center">
+  <v-col cols="12" lg="9" @click="showIcons">
+   <v-card class="mx-auto" max-width="1000" hover :class="{ 'opacity-50': toDo.done }">
+    <v-card-item>
+     <div class="d-flex align-center justify-space-between">
+      <div class="text-lg-h5 text-phone">{{ toDo.title }}</div>
+      <div v-if="toDo.priority === 1">
+       <svg-icon v-if="toDo.priority" type="mdi" :path="path" class="text-green"></svg-icon>
+      </div>
+      <div v-else-if="toDo.priority === 2">
+       <svg-icon v-if="toDo.priority" type="mdi" :path="path" class="text-yellow"></svg-icon>
+      </div>
+      <div v-else-if="toDo.priority === 3">
+       <svg-icon type="mdi" :path="path" class="text-red"></svg-icon>
+      </div>
+     </div>
+    </v-card-item>
+    <v-card-text class="d-flex align-center justify-space-between">
+     <div>{{ toDo.content }}</div>
+     <label class="form-control">
+      <input type="checkbox" :v-model="(doneCheck = toDo.done)" @click="handleEditDone(toDo.id)" :checked="doneCheck" />
+     </label>
+    </v-card-text>
+   </v-card>
+  </v-col>
+  <v-col v-if="showClouds" cols="12" lg="3" class="d-flex flex-column">
+   <v-dialog max-width="600" persistent>
+    <template v-slot:activator="{ props: activatorProps }">
+     <v-btn icon="mdi-pencil" size="default" class="btn-add-todo" v-bind="activatorProps" @click="handleGetToDoInfs(toDo.id)"></v-btn>
+    </template>
 
-     <div v-if="toDo.priority === 1">
-      <svg-icon v-if="toDo.priority" type="mdi" :path="path" class="text-green"></svg-icon>
-     </div>
-     <div v-else-if="toDo.priority === 2">
-      <svg-icon v-if="toDo.priority" type="mdi" :path="path" class="text-yellow"></svg-icon>
-     </div>
-     <div v-else-if="toDo.priority === 3">
-      <svg-icon type="mdi" :path="path" class="text-red"></svg-icon>
-     </div>
-    </div>
-   </v-card-item>
+    <template v-slot:default="{ isActive }">
+     <v-card prepend-icon="mdi mdi-pencil" title="Editar Tarefa">
+      <v-card-text>
+       <v-row>
+        <v-col cols="12" lg="12">
+         <v-text-field label="Título" variant="underlined" clearable required v-model="title" maxlength="40"></v-text-field>
+        </v-col>
+        <v-col cols="12" lg="12">
+         <v-text-field label="Conteúdo" variant="underlined" clearable v-model="content"></v-text-field>
+        </v-col>
+       </v-row>
+       <v-row>
+        <v-col cols="12" lg="12">
+         <v-label> Prioridade </v-label>
+         <v-combobox v-model="priority" :items="['Baixa', 'Média', 'Alta']" variant="underlined"></v-combobox>
+        </v-col>
+       </v-row>
+      </v-card-text>
 
-   <v-card-text class="d-flex align-center justify-space-between">
-    <div>{{ toDo.content }}</div>
-    <label class="form-control">
-     <input type="checkbox" :v-model="(doneCheck = toDo.done)" @click="handleEditDone(toDo.id)" :checked="doneCheck" />
-    </label>
-   </v-card-text>
-  </v-card>
- </v-col>
+      <v-card-actions>
+       <v-btn text="Editar" color="primary" variant="tonal" @click="handlePutToDo(toDo.id), (isActive.value = false)"></v-btn>
+       <v-btn text="Cancelar" @click="isActive.value = false"></v-btn>
+      </v-card-actions>
+     </v-card>
+    </template>
+   </v-dialog>
+   <v-btn class="d-block" icon="mdi-delete" size="default"></v-btn
+  ></v-col>
+ </v-row>
 </template>
 
 <style scoped>
