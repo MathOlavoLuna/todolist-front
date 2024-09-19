bcdd72a0ce7bf3147f1f0f59d1d4165250c5d3ea
Matheus Luna <srmatheus6162@gmail.com>
2024-09-19 20:00:27 -0300

    feat: botando a modal component na home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,6 +9,8 @@ import type { ToDoType } from '@/types';
 import { checkLogged } from '@/utils/checkLogged';
 import { onMounted, ref, watch } from 'vue';
 import { validatePriority } from '@/utils/validatePriority';
+import Modal from '@/components/Modal.vue';
+import type ModalType from '@/types/ModalType';
 
 const toDos = ref<ToDoType[]>();
 //newToDo
@@ -24,111 +26,65 @@ const lastPage = ref<number>(0);
 const spinner = ref<boolean>(false);
 
 const handleGetToDo = async () => {
- const response = await getToDo(page.value);
- lastPage.value = response.last_page;
+  const response = await getToDo(page.value);
+  lastPage.value = response.last_page;
 
- if (response) {
-  toDos.value = response.data;
-  spinner.value = false;
-  return true;
- }
- return false;
+  if (response) {
+    toDos.value = response.data;
+    spinner.value = false;
+    return true;
+  }
+  return false;
 };
 
 const handleGetUser = async () => {
- const response = await getUser();
- localStorage.setItem('user', JSON.stringify(response));
+  const response = await getUser();
+  localStorage.setItem('user', JSON.stringify(response));
 };
 
-const handlePostToDo = async () => {
- const validatedPriority = validatePriority(priority.value);
- const response = await postToDo(title.value, content.value, validatedPriority);
+const handlePostToDo = async (title: string, content: string, priority: string) => {
+  const validatedPriority = validatePriority(priority);
+  const response = await postToDo(title, content, validatedPriority);
 
- if (response) {
-  alert('A fazer atribuído.');
-  handleGetToDo();
- } else {
-  alert('A fazer não atribuído.');
- }
+  if (response) {
+    alert('A fazer atribuído.');
+    handleGetToDo();
+  } else {
+    alert('A fazer não atribuído.');
+  }
 };
+
+const modalHome: ModalType = { icon: 'mdi-plus', modalIcon: 'mdi mdi-playlist-plus', modalTitle: 'Adicionar Tarefa', size: 'large', secondBtnFunction: handlePostToDo };
 
 onMounted(() => {
- spinner.value = true;
- checkLogged('/');
- handleGetUser();
- handleGetToDo();
+  spinner.value = true;
+  checkLogged('/');
+  handleGetUser();
+  handleGetToDo();
 });
 </script>
 
 <template>
- <v-container class="h-100 pa-0 bg-blue" fluid>
-  <v-row>
-   <v-col cols="12" lg="12">
-    <NavBar class="bg-cyan" />
-   </v-col>
-  </v-row>
-  <v-row class="ml-5 align-center">
-   <v-col cols="12" lg="12" md="12">
-    <HelloUser />
-   </v-col>
-  </v-row>
-  <div v-if="spinner" class="d-flex justify-center align-center h-75 w-100">
-   <v-progress-circular color="black" indeterminate :size="59" :width="5"></v-progress-circular>
-  </div>
-  <ToDoCard :to-do="toDo" v-for="toDo in toDos" :key="toDo.id" @call-get-to-do="handleGetToDo" />
+  <v-container class="h-100 pa-0 bg-blue" fluid>
+    <v-row>
+      <v-col cols="12" lg="12">
+        <NavBar class="bg-cyan" />
+      </v-col>
+    </v-row>
+    <v-row class="ml-5 align-center">
+      <v-col cols="12" lg="12" md="12">
+        <HelloUser />
+      </v-col>
+    </v-row>
+    <div v-if="spinner" class="d-flex justify-center align-center h-75 w-100">
+      <v-progress-circular color="black" indeterminate :size="59" :width="5"></v-progress-circular>
+    </div>
+    <ToDoCard :to-do="toDo" v-for="toDo in toDos" :key="toDo.id" @call-get-to-do="handleGetToDo" />
+    <v-row>
+      <v-col cols="12" class="mb-14"> <v-pagination v-model="page" :length="lastPage" rounded="circle" @click="handleGetToDo()"></v-pagination></v-col>
+    </v-row>
 
-  <v-row>
-   <v-col cols="12" class="mb-14"> <v-pagination v-model="page" :length="lastPage" rounded="circle" @click="handleGetToDo()"></v-pagination></v-col>
-  </v-row>
-
-  <v-dialog max-width="600" persistent>
-   <template v-slot:activator="{ props: activatorProps }">
-    <v-btn icon="mdi-plus" size="large" class="btn-add-todo" v-bind="activatorProps"></v-btn>
-   </template>
-
-   <template v-slot:default="{ isActive }">
-    <v-card prepend-icon="mdi mdi-playlist-plus" title="Adicionar Tarefa">
-     <v-card-text>
-      <v-row>
-       <v-col cols="12" lg="12">
-        <v-text-field label="Título" variant="underlined" clearable required v-model="title" maxlength="40"></v-text-field>
-       </v-col>
-       <v-col cols="12" lg="12">
-        <v-text-field label="Conteúdo" variant="underlined" clearable v-model="content"></v-text-field>
-       </v-col>
-      </v-row>
-      <v-row>
-       <v-col cols="12" lg="12">
-        <v-label> Prioridade </v-label>
-        <v-combobox v-model="priority" :items="['Baixa', 'Média', 'Alta']" variant="underlined"></v-combobox>
-       </v-col>
-      </v-row>
-     </v-card-text>
-
-     <v-card-actions>
-      <v-btn text="Adicionar" color="primary" variant="tonal" @click="handlePostToDo(), (isActive.value = false)"></v-btn>
-      <v-btn text="Cancelar" @click="isActive.value = false"></v-btn>
-     </v-card-actions>
-    </v-card>
-   </template>
-  </v-dialog>
- </v-container>
+    <Modal :modal="modalHome" />
+  </v-container>
 </template>
-<style scoped>
-.btn-add-todo {
- position: fixed;
- right: 0;
- bottom: 0;
- z-index: 1;
- margin-right: 20%;
- margin-bottom: 2%;
-}
-
-@media (max-width: 1200px) {
- .btn-add-todo {
-  opacity: 80%;
-  margin-right: 4%;
-  margin-bottom: 6%;
- }
-}
-</style>
+<style scoped></style>
