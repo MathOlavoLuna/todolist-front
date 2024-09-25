ce6a44e835a00b551461ca7f125023725eaf8fe5
Matheus Luna <srmatheus6162@gmail.com>
2024-09-25 10:36:23 -0300

    editei o modal type para algo mais dinâmico e que faça sentido, mas provavelmente vou vir a editar mais ela

--- src/types/ModalType.ts
+++ src/types/ModalType.ts
@@ -1,8 +1,8 @@
+import type ToDoType from './ToDoType';
+
 export default interface ModalType {
   icon: string;
   size: string;
-  firstBtnFunction?: () => void;
   modalIcon: string;
   modalTitle: string;
-  secondBtnFunction: (title: string, content: string, priority: string) => void;
 }
