e70c23f9b85cbb35dfc3dbbf3606b13c40580d75
Matheus Luna <srmatheus6162@gmail.com>
2024-09-25 10:36:41 -0300

    limpei código inutil do checklogged

--- src/utils/checkLogged.ts
+++ src/utils/checkLogged.ts
@@ -3,8 +3,6 @@ import { useRouter } from 'vue-router';
 export function checkLogged(url: string) {
   const logged = localStorage.getItem('token');
   const router = useRouter();
-  console.log(logged);
-
   if (logged) {
     router.push(url);
   } else {
