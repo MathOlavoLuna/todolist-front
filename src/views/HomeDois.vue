3d5affd28ad7f40fa846e7785c2ef45d762977de
Matheus Luna <srmatheus6162@gmail.com>
2024-09-30 13:59:08 -0300

    funciona pelo amor  de deus

---
+++ src/views/HomeDois.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import SideBarComponent from '@/components/SideBarComponent.vue';
+</script>
+
+<template>
+  <v-container fluid class="h-screen pa-0 ma-0">
+    <v-row class="h-100 pa-0 ma-0">
+      <v-col cols="12" lg="2" class="bg-blue-darken-1">
+        <SideBarComponent />
+      </v-col>
+      <v-col cols="12" lg="10" class="bg-yellow-lighten-5"></v-col>
+    </v-row>
+  </v-container>
+</template>
+<style></style>
