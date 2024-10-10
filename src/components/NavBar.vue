4a0b3f9c8e69ae4964cf2eb77deb8abc4149be11
Matheus Luna <srmatheus6162@gmail.com>
2024-10-10 13:45:39 -0300

    basicamente fiz um logout certo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,33 +1,33 @@
 <script setup lang="ts">
-import { mdiArrowLeftDropCircle } from '@mdi/js';
-const icons = { mdiArrowLeftDropCircle };
+import { logout } from '@/services/api';
 
 const handleLogOut = () => {
- localStorage.removeItem('token');
+  logout();
+  localStorage.removeItem('token');
 };
 </script>
 
 <template>
- <nav class="d-flex align-center justify-space-around py-3">
-  <RouterLink to="/login" class="text-lg-h6 text-black d-flex" icon="">
-   <v-icon icon=" mdi-arrow-left-drop-circle" size="large" color="white" @click="handleLogOut"></v-icon>
-  </RouterLink>
-  <img
-   width="64"
-   height="64"
-   src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/external-to-do-list-productivity-flaticons-lineal-color-flat-icons.png"
-   alt="external-to-do-list-productivity-flaticons-lineal-color-flat-icons"
-  />
-  <RouterLink to="/"></RouterLink>
- </nav>
+  <nav class="d-flex align-center justify-space-around py-3">
+    <RouterLink to="/login" class="text-lg-h6 text-black d-flex" icon="">
+      <v-icon icon=" mdi-arrow-left-drop-circle" size="large" color="white" @click="handleLogOut"></v-icon>
+    </RouterLink>
+    <img
+      width="64"
+      height="64"
+      src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/external-to-do-list-productivity-flaticons-lineal-color-flat-icons.png"
+      alt="external-to-do-list-productivity-flaticons-lineal-color-flat-icons"
+    />
+    <RouterLink to="/"></RouterLink>
+  </nav>
 </template>
 
 <style scoped>
 nav {
- height: 100%;
+  height: 100%;
 }
 
 nav > a {
- text-decoration: none;
+  text-decoration: none;
 }
 </style>
