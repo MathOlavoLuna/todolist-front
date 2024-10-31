183b5e1860c6d57eb00a8fec5251f071f700cfc1
Matheus Luna <srmatheus6162@gmail.com>
2024-10-31 16:30:29 -0300

    atualizando as rotas com a nova rota de register e o userType com novas "variaveis"

--- src/types/UserType.ts
+++ src/types/UserType.ts
@@ -3,6 +3,8 @@ import type ToDoType from './ToDoType';
 export default interface UserType {
   id: string;
   name: string;
+  surname: string;
   email: string;
+  profileImage: string;
   todo?: ToDoType;
 }
