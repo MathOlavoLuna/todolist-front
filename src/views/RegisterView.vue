ac5f2214eeb81787f38322b46f724f22adf43037
Matheus Luna <srmatheus6162@gmail.com>
2024-10-31 16:30:56 -0300

    crating the register view

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+<template>
+  <v-container class="h-screen w-screen">
+    <v-row>
+      <v-col cols="12" lg="12">
+        <form max-width="600" max-height="600px">
+          <v-textarea label="Nome" variant="outlined"></v-textarea>
+          <v-textarea label="Email" variant="outlined"></v-textarea>
+          <v-textarea label="Senha" variant="outlined"></v-textarea>
+          <v-file-input label="Imagem de perfil" variant="outlined"></v-file-input>
+        </form>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
